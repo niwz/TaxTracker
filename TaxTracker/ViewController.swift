9be601fa9b3a0fb6edd38e3da4647771917446cd
Nicholas Wong <nic.wzx@gmail.com>
2019-11-16 21:51:46 -0800

    Initial Commit

---
+++ TaxTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TaxTracker
+//
+//  Created by Nicholas Wong on 11/16/19.
+//  Copyright © 2019 Nicholas Wong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
